697210e3fe70b5f6593a7401e4bef28a345e09e4
cesar.izurieta <cesar.izurieta@891216d4-ac20-11dd-ac80-e787e3ad770f>
2008-12-03 17:08:07 +0000

    Better tests

--- trunk/cryo/backends/tests/sqlite_test.py
+++ trunk/cryo/backends/tests/sqlite_test.py
@@ -1,28 +1,16 @@
 import unittest
-import os
-import os.path
-import traceback
 
 from cryo.backends.sqlite import SQLiteBackend
 
 from ...tests import base
 from ...tests import testclasses
 
-_PATH = './test.sqlite'
-
 
 class SQLiteBackendTestCase(unittest.TestCase, base.BackendTestCase):
 
     def setUp(self):
-        try:
-            self.backend = SQLiteBackend(_PATH, modules=[testclasses])
-            self.connection = self.backend.newconnection()
-            if not self.connection.readtables():
-                self.connection.createtables(testclasses.gettables())
-        except Exception, e:
-            self.tearDown()
-            self.fail(traceback.format_exc())
+        self.backend = SQLiteBackend(":memory:", modules=[testclasses])
+        self.connection = self.backend.newconnection()
+        if not self.connection.readtables():
+            self.connection.createtables(testclasses.gettables())
 
-    def tearDown(self):
-        if os.path.exists(_PATH):
-            os.unlink(_PATH)
